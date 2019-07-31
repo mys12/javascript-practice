aa488452d7c4165aa232233fe283d9215269dc34
mys12 <mys62811@naver.com>
2019-07-31 16:31:32 +0900

    practice javascript

---
+++ function-arguments.js
@@ -0,0 +1,4 @@
+function math (a, b, c) {
+    console.log((b*c)+a);
+}
+math(53,61,67);
---
+++ program.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+// const content = fs.readFileSync("./for-loop.js").toString();
+// console.log(content);
+const http = require("http");
+
+console.log("aaaaaaaaaaaaa");
+
+/* http.get("http://www.bu.ac.kr/main_index.jsp", function(res){
+    console.log(res);
+})*/
+/* fs.readFile("./for-loop.js", function(err, data){
+    if (err){
+        console.log("error");
+        return;
+
+    } 
+    console.log(data.toString());
+})*/
+console.log("bbbbbbbbbbbb");
